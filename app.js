f82be1ef326ddc7db31b0952b0d2c39cbf865afa
kennethdammyr <kennethdammyr@gmail.com>
2014-03-30 15:31:09 +0200

    Developing

--- app.js
+++ app.js
@@ -6,11 +6,35 @@ by Kenneth Dammyr, 2014
 var express = require("express");
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+// simple logger
+app.use(function(req, res, next){
+	console.log('%s %s', req.method, req.url);
+	next();
+});
+app.use(express.json()); 
+
+var shopitems = {
+	1:'Brød',
+	2:'Smør',
+	3:'Melk',
+	4:'Appelsinjuice',
+	5:'Mango',
+	6:'Bananer',
+	7:'Pepper'
+}
+
+app.get('/api/shopitems', function(req, res) {
+	res.json(shopitems);
+});
+
+app.post('/api/shopadd', function(req, res) {
+	console.log("Det skjedde noe");
+    var shopitem = req.body.name,
+        color = req.body.color;
 });
 
 var port = Number(process.env.PORT || 5000);
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
