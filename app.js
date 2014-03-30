f9b620ff99c0ba1d3b7206037f969dcb461f26dd
kennethdammyr <kennethdammyr@gmail.com>
2014-03-30 14:27:30 +0200

    Removed unneeded deps

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ by Kenneth Dammyr, 2014
 */
 
 var express = require("express");
-var logfmt = require("logfmt");
 var app = express();
-
-app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
